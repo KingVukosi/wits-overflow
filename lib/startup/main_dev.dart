2d3bf2b04998c0e0b91d533c7ec6c303d25348a7
thefirsthero <kingvukosi@outlook.com>
2021-10-10 01:45:09 +0200

    initialising firebase in main files

--- lib/startup/main_dev.dart
+++ lib/startup/main_dev.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
 import 'package:wits_overflow/startup/wits_overflow_app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await DotEnv.load(fileName: "env/.env_dev");
   runApp(WitsOverflowApp());
 }
